51e527c0181d85cbabd09e146c2e089199dee41c
IgorInfnetTPSeASSESSMENTS <beatifuldeath1@gmail.com>
2024-09-05 11:58:41 -0300

    fix previous-commit

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -6,9 +6,16 @@ export default function DashboardScreen() {
   const router = useRouter();
 
   return (
-    <View>
-      <Text>Dashboard Screen</Text>
-      <Button title="Go to Home" onPress={() => router.push('/home')} />
-    </View>
+    <>
+      <View>
+        <Text>Dashboard Screen</Text>
+        <Button title="Go to Home" onPress={() => router.push('/home')} />
+      </View>
+
+      <View>
+      <Text>New Item Screen</Text>
+      <Button title="Go to New-Item" onPress={() => router.push('/new-item')} />
+      </View>
+    </>
   );
 }
